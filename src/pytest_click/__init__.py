299d201b8c345af2f0a1ba22738454bd471cb21c
Dmitry Dygalo <dadygalo@gmail.com>
2020-08-29 12:36:32 +0200

    chore: Add type annotations & mypy job

--- src/pytest_click/__init__.py
+++ src/pytest_click/__init__.py
@@ -1,7 +1,9 @@
+from _pytest.config import Config
+
 from .fixtures import cli_runner, isolated_cli_runner
 
 
-def pytest_configure(config):
+def pytest_configure(config: Config) -> None:
     config.addinivalue_line(
         "markers",
         "runner_setup(**kwargs): Pass kwargs to `click.testing.CliRunner` initialization.",

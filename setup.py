6f20700c46cab5d0ae7d2edaeb345d58d0bd15ec
Dmitry Dygalo <dadygalo@gmail.com>
2020-08-29 11:51:30 +0200

    refactor!: Drop Python 3.4 support

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
